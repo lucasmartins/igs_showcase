ce067cdb86e1de2e378b6d1cedb7ba87f6b5c147
Lucas N. Martins <lucas.martins@innovit.com.br>
2012-09-04 08:51:05 -0300

    init

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+class MainController < ActionController::Base
+
+  def index
+    
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChartRorTest::Application.config.secret_token = '8d6700aee5fede5c1c90e31201aa6163a824239194544d386e8cb228f2dbd32ff82d07df403a73687e7d797840e40d8f644e76ac2b30b3224f837d4eb1101a9d'
