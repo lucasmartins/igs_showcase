9f787054a0f01d6a7a555755ae1e42bab3b4dbda
Lucas N. Martins <lucas.martins@innovit.com.br>
2012-09-11 12:02:54 -0300

    showcase working again

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,9 @@
+#encoding: utf-8
 class MainController < ApplicationController
 	include Igs
 
   def index    
-    @pie = PieChart.new(200,0.4,'#pie_chart_01',{'Mussarela'=>25,'Brocolli'=>25,'Pepperoni'=>50})
+    @pie = PieChart.new('Título',200,0.4,'#pie_chart_01',{'Mussarela'=>25,'Brocolli'=>25,'Pepperoni'=>50})
 
     respond_to do |format|
       format.html # index.html.erb
