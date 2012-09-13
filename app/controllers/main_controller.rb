009220468ff9534d73f4da8e302bfbc3609270f7
Lucas Neves Martins <snowniak@gmail.com>
2012-09-13 08:15:33 -0300

    wip

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,8 @@ class MainController < ApplicationController
 	include Igs
 
   def index    
-    @pie = PieChart.new(200,0.4,'#pie_chart_01',{'Mussarela'=>25,'Brocolli'=>25,'Pepperoni'=>50})
+    @pie_01 = PieChart.new('Pizzas',200,0.4,'#pie_chart_01',{'Mussarela'=>25,'Brocolli'=>25,'Pepperoni'=>50})
+    @pie_02 = PieChart.new('Programming Languages',200,0.4,'#pie_chart_02',{'C/C++'=>100,'Ruby'=>15,'Python'=>25,'JavaScript'=>80})
 
     respond_to do |format|
       format.html # index.html.erb
